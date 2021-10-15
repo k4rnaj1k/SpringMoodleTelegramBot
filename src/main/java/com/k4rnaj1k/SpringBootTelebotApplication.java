0f08928f83aba88fa965df2455566daf946011c0
k4rnaj1k <vladliasota@gmail.com>
2021-10-16 02:03:09 +0300

    Removed unnecessary import.

--- src/main/java/com/k4rnaj1k/SpringBootTelebotApplication.java
+++ src/main/java/com/k4rnaj1k/SpringBootTelebotApplication.java
@@ -3,6 +3,8 @@ package com.k4rnaj1k;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
+
 @SpringBootApplication
 public class SpringBootTelebotApplication {
     public static void main(String[] args) {
