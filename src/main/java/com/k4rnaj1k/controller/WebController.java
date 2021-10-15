571c0e3cc2666cbf96dec7adf48883e819f1329b
k4rnaj1k <vladliasota@gmail.com>
2021-10-16 02:00:07 +0300

    Added logging, a few endpoints.

--- src/main/java/com/k4rnaj1k/controller/WebController.java
+++ src/main/java/com/k4rnaj1k/controller/WebController.java
@@ -2,7 +2,10 @@ package com.k4rnaj1k.controller;
 
 import com.k4rnaj1k.dto.LoginRequest;
 import com.k4rnaj1k.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 public class WebController {
     private final UserService userService;
+    private final static Logger log = LoggerFactory.getLogger(WebController.class);
 
     public WebController(UserService userService) {
         this.userService = userService;
@@ -21,17 +25,25 @@ public class WebController {
 
     @GetMapping("/login")
     public String login(@RequestParam("chat_id") String chatId, Model model) {
-        System.out.println(chatId);
+        log.info("Login attempt with chatId {}", chatId);
         model.addAttribute("chatId", chatId);
         return "login";
     }
 
     @PostMapping("submit")
-    public String submit(@RequestBody LoginRequest loginRequest, @RequestParam("chat_id") String chatid) {
-//        if(userService.getUser())
-        if (userService.loadUser(loginRequest, chatid))
-            return "success";
-        else
+    public ResponseEntity<Object> submit(@RequestBody LoginRequest loginRequest, @RequestParam("chat_id") String chatId) {
+        if (userService.loadUser(loginRequest, chatId)) {
+            userService.loadUsersFields(chatId);
+            log.info("Successfully loaded user's fields {}.", chatId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            log.error("Encountered an error during process of fields retrieval chatId = {}.", chatId);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("success")
+    public String success(){
+        return "success";
     }
 }
