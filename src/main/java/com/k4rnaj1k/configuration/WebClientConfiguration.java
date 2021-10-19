ba8c4dfa11f247c7061e6ee864a9326896ca81fa
k4rnaj1k <vladliasota@gmail.com>
2021-10-20 01:16:27 +0300

    Edited to builder.

--- src/main/java/com/k4rnaj1k/configuration/WebClientConfiguration.java
+++ src/main/java/com/k4rnaj1k/configuration/WebClientConfiguration.java
@@ -1,17 +1,9 @@
 package com.k4rnaj1k.configuration;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfiguration {
@@ -20,16 +12,7 @@ public class WebClientConfiguration {
 
     @Bean
     public WebClient webClient() {
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                .responseTimeout(Duration.ofMillis(5000))
-                .doOnConnected(conn ->
-                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
-
-        return WebClient.builder()
-                .baseUrl(moodleUrl)
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
+        return WebClient.builder().baseUrl(moodleUrl).build();
     }
+
 }
