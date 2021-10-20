bde88934808cbd6e79884a1cc57c14ac2a10c9b5
k4rnaj1k <vladliasota@gmail.com>
2021-10-20 17:51:55 +0300

    Bugfix

--- src/main/java/com/k4rnaj1k/receiver/UpdateReceiver.java
+++ src/main/java/com/k4rnaj1k/receiver/UpdateReceiver.java
@@ -67,7 +67,7 @@ public class UpdateReceiver {
     }
 
     private boolean isFromChatUser(Update update) {
-        return (update.getMessage().getChat().getType().equals("group")||update.getMessage().getChat().getType().equals("supergroup")) && (update.hasMessage());
+        return (update.getMessage().getChat().getType().equals("group")||update.getMessage().getChat().getType().equals("supergroup")) && (update.getMessage().getForwardFromChat()==null && update.getMessage().getForwardFrom()==null) && (update.hasMessage());
     }
 
     private boolean isChatStatusUpdate(Update update) {
